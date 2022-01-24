63792be127d7996e1135dfd5a065884e301c9780
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-01-24 17:36:04 -0600

    Curso angular | Enviando informacion a un componente hijo, y emitiendo eventos desde el componente hijo... Fin del curso

---
+++ src/app/pages/posts/post/post.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() mensaje:any;
+  @Output() clickPost = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    //console.log( this.mensaje.id )
+    this.clickPost.emit( this.mensaje.id );
+  }
+
+}
